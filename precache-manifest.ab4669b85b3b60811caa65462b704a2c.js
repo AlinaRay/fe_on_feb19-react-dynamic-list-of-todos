c01de4ac5b85fa721d9c8d90a1b5d1b735852329
Alina Ray <a.alina.ray@gmail.com>
2019-07-01 19:15:32 +0300

    Updates

--- precache-manifest.ab4669b85b3b60811caa65462b704a2c.js
+++ precache-manifest.ab4669b85b3b60811caa65462b704a2c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "15805b3c2121e75b97a91941082b5839",
+    "revision": "9123bcd02c367d0e731be7b798d0ade0",
     "url": "/fe_on_feb19-react-dynamic-list-of-todos/index.html"
   },
   {
-    "revision": "59c7ab594f4cc08dcd49",
+    "revision": "1de54488c6c9a5770a48",
     "url": "/fe_on_feb19-react-dynamic-list-of-todos/static/css/main.04eb9f34.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fe_on_feb19-react-dynamic-list-of-todos/static/js/2.663f6d43.chunk.js"
   },
   {
-    "revision": "59c7ab594f4cc08dcd49",
-    "url": "/fe_on_feb19-react-dynamic-list-of-todos/static/js/main.4fbf2c4a.chunk.js"
+    "revision": "1de54488c6c9a5770a48",
+    "url": "/fe_on_feb19-react-dynamic-list-of-todos/static/js/main.a6ec31ca.chunk.js"
   },
   {
     "revision": "5295d3021697f9675dbe",
